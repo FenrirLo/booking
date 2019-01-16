dffbdcb0ff4f7fbe2ef234f05f65e33eaace82ab
Drew Yoxsimer and fen lombardi <fen@>
2019-01-16 14:30:00 -0800

    add framework for modal ontop of booking funtionality.

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,24 @@
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
-    $("#person").append("blah blah");
-    $("#description").append("blah blah");
-    $("#date").append("blah blah");
-    $("#timeStart").append("blah blah");
-    $("#timeEnd").append("blah blah");
+    var personInput = $("input#person").val();
+    var descriptionInput = $("input#description").val();
+    var dateInput = $("input#date").val();
+    var timeStartInput = $("input#timeStart").val();
+    var timeEndInput = $("input#timeEnd").val();
+
+    $(".person").text(personInput);
+    $(".description").text(descriptionInput);
+    $(".date").text(dateInput);
+    $(".timeStart").text(timeStartInput);
+    $(".timeEnd").text(timeEndInput);
 
     $("#confirm").show();
 
     event.preventDefault();
+    });
+
+  $(".btnModal").click(function(){
+    $("#myModal").modal();
   });
-})
+
+});
